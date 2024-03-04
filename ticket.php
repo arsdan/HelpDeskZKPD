72d388f2ddd2791a2677284e483f898c5f9d7ade
unknown <dasiberchik@gmail.com>
2024-03-04 16:28:33 +0700

    Add Insert ticket

--- ticket.php
+++ ticket.php
@@ -1,50 +1,53 @@
 <?php
 //подключение к postgres
-$connect_db = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres");
-if($connect_db){
-	echo 'connect';
+$host = 'localhost';
+$port = '5432';
+$dbname = 'postgres';
+$user = 'postgres';
+$connect_db = pg_connect('host='.$host.' port='.$port.' dbname='.$dbname.' user='.$user);
+if(!$connect_db){
+	die('Ошбика подключения к базе данных ');
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['insert_ticket'])) {
-$sql_select_topics = 'select * from topic_tickets';
-$query_get_topic = pg_query($connect_db, $sql_select_topics);
-;
-$sql_select_staff = '
-select user_id, user_name, user_last_name
-from user_t
-where user_is_staff = true';
-$query_get_staff = pg_query($connect_db, $sql_select_staff);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (!empty($_POST['ticket_description']) && !empty($_POST['ticket_staff']) && !empty($_POST['topic_ticket'])) {
 
-//Добавление тикета
+  // SQL запросы
+    $sql_select_topics = 'select * from topic_tickets';
+    $query_get_topic = pg_query($connect_db, $sql_select_topics);
+    $sql_select_staff = '
+    select user_id, user_name, user_last_name
+    from user_t
+    where user_is_staff = true';
+    $query_get_staff = pg_query($connect_db, $sql_select_staff);
 
+// Записываем id значений
     $ticket_client = 1; //нужно добавить авторизацию
   //$ticket_comment =
     $ticket_description = $_REQUEST['ticket_description'];
-    while($row -> pg_fetch_object($query_get_staff)){
-    	if($query_get_staff->user_name.$row->user_last_name == $_REQUEST['ticket_staff']){
+    while($row = pg_fetch_object($query_get_staff)){
+    	if($row->user_name.$row->user_last_name == $_REQUEST['ticket_staff']){
     		$ticket_staff_id = $row->user_id;
     	}
     }
-    echo $ticket_staff_id;
-    while($row -> pg_fetch_object($query_get_topic)){
-    	if($query_get_topic->topic_ticket_name == $_REQUEST['topics_ticket']){
+    while($row = pg_fetch_object($query_get_topic)){
+    	if($row->topic_ticket_name == $_REQUEST['topic_ticket']){
     		$topic_ticket_id = $row->topic_ticket_id;
-    	};
+    	}
     }
-    echo $topic_ticket_id;
 
-    // $sql_insert_ticket = 'INSERT INTO tickets (ticket_description, topic_ticket_id, ticket_client, ticket_staff) VALUES (?,?,1,?)';
-    // $stmt = $connect_db->prepare($sql_insert_ticket);
-    //$stmt->bind_param('iii', $ticket_description, $topic_ticket_id, $ticket_staff_id);
-    // $stmt->execute([$ticket_description, $topic_ticket_id, $ticket_staff_id]);
-    $sql_insert_ticket = 'INSERT INTO tickets (ticket_description, topic_ticket_id, ticket_client, ticket_staff)
-    VALUES (sqwqweqewrewtty,1,1,2)';
-    pg_query($connect_db, $sql_insert_ticket);
-    if($connect_db->query($sql_insert_ticket) === true){
-    	echo 'добавлено';
-    }
-    else {
-    	echo 'не добавлено';
-    }
+//Добавление тикета в бд
+    $sql_insert_ticket = 'INSERT INTO tickets (ticket_description, topic_ticket_id, ticket_client, ticket_staff) VALUES ($1,$2,$3,$4)';
+    $stmt = pg_prepare($connect_db, "my_query", $sql_insert_ticket);
+    $stmt = pg_execute($connect_db, "my_query", [$ticket_description, $topic_ticket_id,1, $ticket_staff_id]);
+    header('Location: index.php');
+    exit();
+  }
+  else {
+    echo 'Пустые поля';
+  }
+}
+else {
+  echo 'Ошибка добавления';
 }
 ?>
